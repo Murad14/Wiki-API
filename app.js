c8a3b8e6959d5a42be2353fa8452a40ed8873bc8
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-30 14:21:38 +0600

    modified

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
-
+mongoose.set('strictQuery', true);
 const app = express();
 
 app.use(express.json());
@@ -13,50 +13,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://127.0.0.1:27017/wikiDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 const articleSchema = new mongoose.Schema({
     title: {
-      type: String,
-    //   required: true
+        type: String,
+        required: true
     },
     content: {
-      type: String,
-    //   required: true
+        type: String,
+        required: true
     }
 })
 const Article = mongoose.model('Article', articleSchema);
 
-app.get("/articles", function(req, res){
-    Article.find(function(err, foundArticles){
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(foundArticles);
-        }
+app.route('/articles')
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(foundArticles);
+            }
+        })
     })
-});
+    .post(function (req, res) {
+        console.log();
+        console.log();
 
-app.post("/articles", function(req,res){
-    console.log();
-    console.log();
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
 
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article.")
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles")
+            } else {
+                res.send(err);
+            }
+        })
+    })
 
-    newArticle.save(function(err){
-        if(!err){
-            res.send("Successfully added a new article.")
-        }else{
-            res.send(err);
-        }
-    });
-})
+
 
 app.listen(3000, () => {
-  console.log('App listening on port 3000');
+    console.log('App listening on port 3000');
 });
